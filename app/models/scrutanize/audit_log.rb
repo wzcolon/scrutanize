0c077e3ed38c7bb50fde0fc3950591ad693e9c1d
Zach Colon <zcolon80@gmail.com>
2016-02-02 20:37:38 -0500

    support prior versions of rails

--- app/models/scrutanize/audit_log.rb
+++ app/models/scrutanize/audit_log.rb
@@ -1,5 +1,9 @@
 module Scrutanize
   class AuditLog < ActiveRecord::Base
+    if Rails.version.to_i < 4
+      attr_accessible :audit_type, :message
+    end
+
     validates :audit_type, presence: true
   end
 end
