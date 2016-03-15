f6118899f178d7654328423fd9f9b62f89a2b42d
Zach Colon <zcolon80@gmail.com>
2016-03-14 20:17:39 -0400

    fix spelling mistake

--- lib/generators/scrutanize/views_generator.rb
+++ lib/generators/scrutanize/views_generator.rb
@@ -4,7 +4,7 @@ module Scrutanize
     source_root Engine.root.join('app')
 
     def copy_views
-      copy_file "controllers/scrutanize/audit_logs_controller.rb", "app/controllers/scutanize/audit_logs_controller.rb"
+      copy_file "controllers/scrutanize/audit_logs_controller.rb", "app/controllers/scrutanize/audit_logs_controller.rb"
 
       copy_file "views/scrutanize/audit_logs/index.html.haml", "app/views/scrutanize/audit_logs/index.html.haml.rb"
     end
