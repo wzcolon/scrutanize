9690849eb9d9f64b8b2fecf6a317f7ea0bc78a99
Zach Colon <zcolon80@gmail.com>
2016-05-17 18:25:16 -0400

    [bugfix] attr_accessible for rails 3

--- app/models/scrutanize/audit_report.rb
+++ app/models/scrutanize/audit_report.rb
@@ -2,6 +2,10 @@ module Scrutanize
   class AuditReport < ActiveRecord::Base
     include Scrutanize::Concerns::Models::DeleteableBehavior
 
+    if Rails.version.to_i < 4
+      attr_accessible :name, :deleted_at, :records_audited
+    end
+
     paginates_per Scrutanize.configuration.defaults_per_page
 
     has_many :audit_logs, class_name: Scrutanize::AuditLog, dependent: :destroy
